8c197e7184d12ef23ab5ba503a0c4089c3a34d7b
Arrcady555 <parfenov7233@gmail.com>
2022-05-16 20:51:55 +0300

    Преобразование даты-2

--- src/main/java/ru/job4j/grabber/utils/HarbCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HarbCareerDateTimeParser.java
@@ -3,25 +3,13 @@ package ru.job4j.grabber.utils;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZonedDateTime;
 
 public class HarbCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        String[] dateTime = parse.split("T");
-        String[] date = dateTime[0].split("-");
-        String[] time = dateTime[1].split(":", 3);
-
-        return LocalDateTime.of(
-                LocalDate.of(
-                        Integer.parseInt(date[0]),
-                        Integer.parseInt(date[1]),
-                        Integer.parseInt(date[2])),
-                LocalTime.of(
-                        Integer.parseInt(time[0]),
-                        Integer.parseInt(time[1]),
-                        Integer.parseInt(time[2])
-                )
-        );
+        ZonedDateTime time = ZonedDateTime.parse(parse);
+        return time.toLocalDateTime();
     }
 }
