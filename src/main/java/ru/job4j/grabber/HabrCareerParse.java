ad45f9d3e39b874f65e896ba6fb3f43ac6f89cee
Arrcady555 <parfenov7233@gmail.com>
2022-05-16 22:15:39 +0300

    Парсинг https://www.sql.ru/forum/job-offers/3 -3

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -9,15 +9,14 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class HabrCareerParse {
-    private static int num;
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=" + num, SOURCE_LINK);
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        for (num = 1; num <= 5; num++) {
-            Connection connection = Jsoup.connect(PAGE_LINK);
+        for (int num = 1; num <= 5; num++) {
+            Connection connection = Jsoup.connect(PAGE_LINK + num);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
