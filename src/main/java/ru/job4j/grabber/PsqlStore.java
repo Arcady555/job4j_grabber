acf21c1c3299e84137fc2e0bd80853c51b987b32
Arrcady555 <parfenov7233@gmail.com>
2022-05-19 22:06:06 +0300

    PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,121 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.utils.Store;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection cnn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+            String url = cfg.getProperty("url");
+            String login = cfg.getProperty("login");
+            String password = cfg.getProperty("password");
+            cnn = DriverManager.getConnection(url, login, password);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "insert into post(name, link, text, created) values (?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getLink());
+            statement.setString(3, post.getDescription());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    post.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        ArrayList<Post> list = new ArrayList<>();
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "select * from post")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    list.add(newPost(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "select * from post where id =" + id)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    post = newPost(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    private Post newPost(ResultSet resultSet) throws SQLException {
+        return new Post(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("text"),
+                resultSet.getString("link"),
+                resultSet.getTimestamp("created").toLocalDateTime()
+        );
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties prop = new Properties();
+        try (InputStream in = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("grabber.properties")) {
+            prop.load(in);
+            try (PsqlStore psqlStore = new PsqlStore(prop)) {
+                psqlStore.save(new Post(
+                        "senior",  "www.excellent.com", "excellent job!",
+                        LocalDateTime.now()));
+                psqlStore.save(new Post(
+                        "middle",  "www.good.com", "good job!",
+                        LocalDateTime.of(2020, 4, 15, 12, 40, 35)));
+                psqlStore.save(new Post(
+                        "junior",  "www.dream.com", "dream job!!!!!",
+                        LocalDateTime.of(2021, 2, 28, 2, 3, 4)));
+                List<Post> list = psqlStore.getAll();
+                for (Post post : list) {
+                    System.out.println(post.toString());
+                }
+                System.out.println(psqlStore.findById(17).toString());
+            }
+        }
+    }
+}
