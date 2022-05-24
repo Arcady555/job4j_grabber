e5409ebeb3ebc22de02157647609c9ac50b00bba
Arcady555 <79311106+Arcady555@users.noreply.github.com>
2022-05-24 09:09:14 +0300

    Add files via upload

--- per_comp.sql
+++ per_comp.sql
@@ -8,9 +8,7 @@ select c.name, count(p.company_id) from company as c
   group by c.name
 having  count(p.company_id) =
     (select max(v.count) from (
-        select c.name, count(p.company_id) from company as c
-          join person as p
-          on p.company_id = c.id
-          group by c.name
-        ) as v
+         select company_id, count(*) from person
+         group by company_id
+         ) as v
     );
