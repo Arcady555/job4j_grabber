f941b33df782cb71d154d0715eb500041c56ff11
Arrcady555 <parfenov7233@gmail.com>
2022-07-01 15:21:58 +0300

    Поддержка форматом XML, JSON в генераторе отчетов. [#851 #317212]-2

--- src/main/java/ru/job4j/ood/task/ReportEngineInJSON.java
+++ src/main/java/ru/job4j/ood/task/ReportEngineInJSON.java
@@ -6,17 +6,18 @@ import java.util.function.Predicate;
 public class ReportEngineInJSON implements Report {
 
     private Store store;
+    ObjectMapper mapper;
 
     public ReportEngineInJSON(Store store) {
         this.store = store;
+        mapper = new ObjectMapper();
+        mapper.setDateFormat(ConstantValues.DATE_FORMAT_FOF_SERIALISATION);
     }
 
     @Override
     public String generate(Predicate<Employee> filter) {
         StringBuilder text = new StringBuilder();
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.setDateFormat(ConstantValues.DATE_FORMAT_FOF_SERIALISATION);
             String json = mapper.writeValueAsString(new Employees(store.findBy(filter)));
             text.append(json);
         } catch (Exception e) {
--- src/main/java/ru/job4j/ood/task/ReportEngineInXML.java
+++ src/main/java/ru/job4j/ood/task/ReportEngineInXML.java
@@ -10,26 +10,27 @@ import java.util.function.Predicate;
 public class ReportEngineInXML implements Report {
 
     private Store store;
+    Marshaller marshaller;
 
     public ReportEngineInXML(Store store) {
         this.store = store;
+        try {
+            JAXBContext context = JAXBContext.newInstance(Employees.class);
+            marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        } catch (JAXBException e1) {
+            e1.printStackTrace();
+        }
     }
 
     @Override
     public String generate(Predicate<Employee> filter) {
         StringBuilder text = new StringBuilder();
-        try {
-            JAXBContext context = JAXBContext.newInstance(Employees.class);
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-            try (StringWriter writer = new StringWriter()) {
-                marshaller.marshal(new Employees(store.findBy(filter)), writer);
-                text.append(writer);
-            } catch (IOException e2) {
-                e2.printStackTrace();
-            }
-        } catch (JAXBException e1) {
-            e1.printStackTrace();
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(new Employees(store.findBy(filter)), writer);
+            text.append(writer);
+        } catch (IOException | JAXBException e2) {
+            e2.printStackTrace();
         }
         return text.toString();
     }
