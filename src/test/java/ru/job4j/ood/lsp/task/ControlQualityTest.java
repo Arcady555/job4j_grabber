a8ca032bdc298aec521a9fced6b94302a3f12ec8
Arrcady555 <parfenov7233@gmail.com>
2022-07-02 20:13:35 +0300

    Хранилище продуктов [#852]-2

--- src/test/java/ru/job4j/ood/lsp/task/ControlQualityTest.java
+++ src/test/java/ru/job4j/ood/lsp/task/ControlQualityTest.java
@@ -8,21 +8,6 @@ import java.util.GregorianCalendar;
 import static org.junit.Assert.*;
 
 public class ControlQualityTest {
-    @Test
-    public void whenInOneStore() {
-        Food milk = new Milk("Доярушка");
-        milk.setCreateDate(new GregorianCalendar(2022, 4, 25));
-        milk.setExpiryDate(new GregorianCalendar(2022, 7, 25));
-        Food bread = new Bread("Полюшко");
-        bread.setCreateDate(new GregorianCalendar(2022, 5, 16));
-        bread.setExpiryDate(new GregorianCalendar(2022, 6, 16));
-        ControlQuality controlQuality = new ControlQuality();
-        controlQuality.whatWithFood(milk);
-        controlQuality.whatWithFood(bread);
-        Assert.assertEquals(controlQuality.getStoreSetList().get(0).getClass(), Shop.class);
-        Assert.assertEquals(milk, controlQuality.getStoreSetList().get(0).getList().get(0));
-        Assert.assertEquals(bread, controlQuality.getStoreSetList().get(0).getList().get(1));
-    }
 
     @Test
     public void whenInTwoStore() {
@@ -32,12 +17,30 @@ public class ControlQualityTest {
         Food food1 = new Bread("Полюшко");
         food1.setCreateDate(new GregorianCalendar(2022, 5, 16));
         food1.setExpiryDate(new GregorianCalendar(2022, 6, 16));
-        ControlQuality controlQuality = new ControlQuality();
-        controlQuality.whatWithFood(food);
-        controlQuality.whatWithFood(food1);
-        Assert.assertEquals(controlQuality.getStoreSetList().get(0).getClass(), Trash.class);
-        Assert.assertEquals(controlQuality.getStoreSetList().get(1).getClass(), Shop.class);
-        Assert.assertEquals(food, controlQuality.getStoreSetList().get(0).getList().get(0));
-        Assert.assertEquals(food1, controlQuality.getStoreSetList().get(1).getList().get(0));
+        ControlQuality controlQuality1 = new ControlQuality();
+        controlQuality1.whatWithFood(food);
+        controlQuality1.whatWithFood(food1);
+        Assert.assertEquals(controlQuality1.getStoreSetList().get(0).getClass(), Trash.class);
+        Assert.assertEquals(controlQuality1.getStoreSetList().get(1).getClass(), Shop.class);
+        Assert.assertEquals(food, controlQuality1.getStoreSetList().get(0).getList().get(0));
+        Assert.assertEquals(food1, controlQuality1.getStoreSetList().get(1).getList().get(0));
+        controlQuality1.getStoreSetList().get(0).getList().remove(0);
+        controlQuality1.getStoreSetList().get(1).getList().remove(0);
+    }
+
+    @Test
+    public void whenInOneStore() {
+        Food milk = new Milk("Доярушка");
+        milk.setCreateDate(new GregorianCalendar(2022, 4, 25));
+        milk.setExpiryDate(new GregorianCalendar(2022, 7, 25));
+        Food bread = new Bread("Полюшко");
+        bread.setCreateDate(new GregorianCalendar(2022, 5, 16));
+        bread.setExpiryDate(new GregorianCalendar(2022, 6, 16));
+        ControlQuality controlQuality2 = new ControlQuality();
+        controlQuality2.whatWithFood(milk);
+        controlQuality2.whatWithFood(bread);
+        Assert.assertEquals(controlQuality2.getStoreSetList().get(0).getClass(), Shop.class);
+        Assert.assertEquals(milk, controlQuality2.getStoreSetList().get(0).getList().get(0));
+        Assert.assertEquals(bread, controlQuality2.getStoreSetList().get(0).getList().get(1));
     }
 }
