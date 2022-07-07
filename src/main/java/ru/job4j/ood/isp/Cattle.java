53595bd98cd5271fa3e40b937a746860918967bc
Arrcady555 <parfenov7233@gmail.com>
2022-07-07 23:04:24 +0300

    Принцип разделения интерфейсов [#4916]

---
+++ src/main/java/ru/job4j/ood/isp/Cattle.java
@@ -0,0 +1,103 @@
+package ru.job4j.ood.isp;
+
+public interface Cattle {
+    String makeFoodFor();
+
+    void shepherd();
+
+    void milking();
+
+    void riding();
+}
+
+class Cow implements Cattle {
+
+    @Override
+    public String makeFoodFor() {
+        return null;
+    }
+
+    @Override
+    public void shepherd() {
+
+    }
+
+    @Override
+    public void milking() {
+
+    }
+
+    @Override
+    public void riding() {
+
+    }
+}
+
+class Rabbit implements Cattle {
+
+    @Override
+    public String makeFoodFor() {
+        return null;
+    }
+
+    @Override
+    public void shepherd() {
+
+    }
+
+    @Override
+    public void milking() {
+
+    }
+
+    @Override
+    public void riding() {
+
+    }
+}
+
+class Elephant implements Cattle {
+
+    @Override
+    public String makeFoodFor() {
+        return null;
+    }
+
+    @Override
+    public void shepherd() {
+
+    }
+
+    @Override
+    public void milking() {
+
+    }
+
+    @Override
+    public void riding() {
+
+    }
+}
+
+class Horse implements Cattle {
+
+    @Override
+    public String makeFoodFor() {
+        return null;
+    }
+
+    @Override
+    public void shepherd() {
+
+    }
+
+    @Override
+    public void milking() {
+
+    }
+
+    @Override
+    public void riding() {
+
+    }
+}
