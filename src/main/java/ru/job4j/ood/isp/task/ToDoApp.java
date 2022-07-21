56aa49830edc1e58cc8ece992ef40827a886026b
Arrcady555 <parfenov7233@gmail.com>
2022-07-21 19:54:13 +0300

    Создать меню. [#4748 #323128]-3

--- src/main/java/ru/job4j/ood/isp/task/ToDoApp.java
+++ src/main/java/ru/job4j/ood/isp/task/ToDoApp.java
@@ -3,26 +3,43 @@ package ru.job4j.ood.isp.task;
 import java.util.Scanner;
 
 public class ToDoApp {
+    public static final String GREETINGS = "Hello, Guy! What are your plans for today?\n";
+    public static final String CHOOSE = System.lineSeparator()
+            + "Choose menu:\n1- add item\n2-print menu\n3-exit";
+    public static final String WHAT_PARENT = "Enter parent";
+    public static final String WHAT_CHILD = "Enter child";
+    public static final String STOP_WORD = "no";
+
     public static void main(String[] args) {
-        System.out.println(ConstantValues.GREETINGS);
+        System.out.println(GREETINGS);
         Menu menu = new SimpleMenu();
-        MenuPrinter menuPrinter = new HardMenuPrinter();
         ToDoApp toDoApp = new ToDoApp();
         toDoApp.toDo(menu);
-        menuPrinter.print(menu);
     }
 
     private void toDo(Menu menu) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println(ConstantValues.WHAT_PARENT);
-            String parent = scanner.nextLine();
-            if (parent.equals(ConstantValues.STOP_WORD)) {
-                break;
+            System.out.println(CHOOSE);
+            String number = scanner.nextLine();
+            switch (number) {
+                case "1":
+                    System.out.println(WHAT_PARENT);
+                    String parent = scanner.nextLine();
+                    System.out.println(WHAT_CHILD);
+                    String child = scanner.nextLine();
+                    menu.add(parent, child, ConstantValues.STUB_ACTION);
+                    break;
+                case "2":
+                    MenuPrinter menuPrinter = new HardMenuPrinter();
+                    menuPrinter.print(menu);
+                    break;
+                case "3":
+                    return;
+                default:
+                    System.out.println("Wrong number!");
+                    break;
             }
-            System.out.println(ConstantValues.WHAT_CHILD);
-            String child = scanner.nextLine();
-            menu.add(parent, child, ConstantValues.STUB_ACTION);
         }
     }
 }
