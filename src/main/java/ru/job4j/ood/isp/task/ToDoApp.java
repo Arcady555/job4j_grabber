7f700617da04e0f145202fe2d6d89be1c9e0cad0
Arrcady555 <parfenov7233@gmail.com>
2022-07-22 12:23:14 +0300

    Создать меню. [#4748 #323128]-4

--- src/main/java/ru/job4j/ood/isp/task/ToDoApp.java
+++ src/main/java/ru/job4j/ood/isp/task/ToDoApp.java
@@ -3,12 +3,20 @@ package ru.job4j.ood.isp.task;
 import java.util.Scanner;
 
 public class ToDoApp {
-    public static final String GREETINGS = "Hello, Guy! What are your plans for today?\n";
-    public static final String CHOOSE = System.lineSeparator()
-            + "Choose menu:\n1- add item\n2-print menu\n3-exit";
+    public static final String GREETINGS = "Hello, Guy! What are your plans for today?"
+            + System.lineSeparator();
+    public static final String CHOOSE = """
+            Choose menu:
+            1 -add item
+            2 -print menu
+            3 -exit
+            """;
     public static final String WHAT_PARENT = "Enter parent";
     public static final String WHAT_CHILD = "Enter child";
-    public static final String STOP_WORD = "no";
+    public static final String ADD_ITEM = "1";
+    public static final String PRINT_MENU = "2";
+    public static final String EXIT = "3";
+    public static final String DEFAULT = "Wrong number!";
 
     public static void main(String[] args) {
         System.out.println(GREETINGS);
@@ -18,26 +26,26 @@ public class ToDoApp {
     }
 
     private void toDo(Menu menu) {
+        MenuPrinter menuPrinter = new HardMenuPrinter();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println(CHOOSE);
             String number = scanner.nextLine();
             switch (number) {
-                case "1":
+                case ADD_ITEM:
                     System.out.println(WHAT_PARENT);
                     String parent = scanner.nextLine();
                     System.out.println(WHAT_CHILD);
                     String child = scanner.nextLine();
                     menu.add(parent, child, ConstantValues.STUB_ACTION);
                     break;
-                case "2":
-                    MenuPrinter menuPrinter = new HardMenuPrinter();
+                case PRINT_MENU:
                     menuPrinter.print(menu);
                     break;
-                case "3":
+                case EXIT:
                     return;
                 default:
-                    System.out.println("Wrong number!");
+                    System.out.println(DEFAULT);
                     break;
             }
         }
